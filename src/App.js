0fc969f4aaf98984d378486cad0394294dca37c0
sksuzanisal <suzanislam2017@gmail.com>
2022-03-16 23:29:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Conter></Conter>
+      <LoadComment></LoadComment>
+
     </div>
   );
 }
+function Comment(props) {
+  return (
+    <div>
+      <h4>{props.email}</h4>
+      <p>{props.body}</p>
+    </div>
+  )
+}
 
+
+function LoadComment() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(data => setComments(data))
+  }, [])
+
+  return (
+    <div>
+      <h2>comments:{comments.length}</h2>
+      {
+        comments.map(comment => <Comment email={comment.email} body={comment.body}></Comment>)
+      }
+    </div>
+  )
+}
+
+
+
+function Conter() {
+  const [count, setCount] = useState(0);
+  const handelIncrease = () => setCount(count + 1);
+  const handelDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Conter:{count}</h1>
+      <button onClick={handelIncrease}>Increase</button>
+      <button onClick={handelDecrease}>Decrease</button>
+    </div>
+  )
+
+
+
+}
 export default App;
